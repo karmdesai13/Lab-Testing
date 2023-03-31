fc05174bb748fe84bdef42e02f140d833d518605
karmdesai13 <119353865+karmdesai13@users.noreply.github.com>
2023-03-31 08:31:32 -0600

    Add files via upload

---
+++ MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Testing
+{
+    public class MathOperations
+    {
+        public double Add(double a, double b) { return a + b; }
+
+        public double Subtract(double a, double b) {  return a - b; }
+
+        public double Multiply(double a, double b) { return a * b; }
+
+        public double Divide(double a, double b) 
+        {
+            if(b==0)
+               throw new DivideByZeroException();    
+            return a / b; 
+        }
+    }
+}
